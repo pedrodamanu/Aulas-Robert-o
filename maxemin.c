9504eea55578a9eab1e751e1ea2ea86cfdb7a389
pedrodamanu <pedro.r.m@outlook.com>
2024-08-26 13:05:58 -0300

    exercicio vetor

--- maxemin.c
+++ maxemin.c
@@ -3,12 +3,19 @@
 
 int main ()
 {
-    int vet[N], i, aux1, aux2;
+    int vet[N], i, aux1 = -2147483648, aux2 = 2147483647;
     printf("Digite 10 elementos: \n");
     for (i = 0; i < N; i++)
     {
         scanf("%d", &vet[i]);
-        aux1 = vet[i];
+        if (aux1 < vet[i])
+        {
+            aux1 = vet[i];
+        }
+        if (aux2 > vet[i])
+        {
+            aux2 = vet[i];
+        }
     }
-    printf("A media eh: %d\n");
+    printf("O menor numero eh %d e o maior eh %d\n", aux2, aux1);
 }
