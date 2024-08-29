7846e0c6c920f1c65c6d921f8393c4f7940e6be8
pedrodamanu <pedro.r.m@outlook.com>
2024-08-29 14:05:24 -0300

    aula matriz

---
+++ aulamatriz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define L 5
+#define C 5
+
+int main ()
+{
+    int m[L][C], i, j;
+    m[0][0] = 2;
+    for (i = 0; i < L; i++)
+    {
+        for(j = 0; j < C; j++)
+        {
+            scanf("%d", &m[i][j]);
+        }
+    }
+} 
