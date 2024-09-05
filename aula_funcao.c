e6af3295735393f77455a7e0a4810fe8ee1eea75
pedrodamanu <pedro.r.m@outlook.com>
2024-09-05 14:57:34 -0300

    aula funcao

---
+++ aula_funcao.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int soma(int a, int b);
+
+
+int main() // TODA FUNÇÃO TEM UMA RESPONSABILIDADE UNICA
+{
+    int x, y, z, t;
+    x = soma(3, 2);
+    y = soma(5, 10);
+    z = soma(x, y);
+    t = soma(x, y + 1); // t em main eh diferente de t em soma
+    printf("%d %d %d %d", x, y, z, t);
+}
+
+// tipo_retorno nome_funcao(parametros)
+// {
+
+// }
+
+int soma(int a, int b)
+{
+    int t = a + b;
+    return t;
+}
