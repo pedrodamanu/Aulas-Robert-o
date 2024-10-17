ad9b69ab1b853d908ac960b1a8dd67bd05dd2790
pedrodamanu <pedro.r.m@outlook.com>
2024-10-17 15:26:00 -0300

    atividade struct

--- atv_struct.c
+++ atv_struct.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #define NT 4
 
+
 typedef struct
 {
     char *nome_mat;
@@ -15,42 +16,89 @@ typedef struct
     int numMat;
 } TAluno;
 
+TAluno criaAluno();
+int criaTurma();
+void printaAluno();
+
+TAluno *info24;
+int numAluno;
+
 int main()
 {
-    TAluno *info24;
-    int i, j, k, numAluno;
+    if(criaTurma() == 0)
+    {
+        printf("VALOR INVALIDO!\n");
+    }
+    printf("\n\n");
+    printaAluno();
+    free(info24);
+}
+
+int criaTurma()
+{
+    int i;
     char strAux[100]; 
     printf("BEM VINDO!!!!\n");
     printf("Digite o numero de alunos: ");
     scanf("%d", &numAluno);
-    if(numAluno > 0)
+    if(numAluno <= 0)
     {
-        info24 = (TAluno *)malloc(numAluno * sizeof(TAluno));
-        for(i = 0; i < numAluno; i++)
+        return 0;
+    }
+    info24 = (TAluno *)malloc(numAluno * sizeof(TAluno));
+    for(i = 0; i < numAluno; i++)
+    {
+        info24[i] = criaAluno();
+    }
+}
+TAluno criaAluno()
+{
+    TAluno aluno;
+    char strAux[100];
+    int i, j, k;
+    __fpurge(stdin);
+    printf("Digite o nome do aluno\n");
+    gets(strAux);
+    __fpurge(stdin);
+    aluno.nome = (char *)malloc((strlen(strAux) + 1) * sizeof(char));
+    strcpy(aluno.nome, strAux);
+    printf("Digite o numero de disciplinas: ");
+    scanf("%d", &aluno.numMat);
+    aluno.grade = (TMateria *)malloc(aluno.numMat * sizeof(TMateria));
+    for(j = 0; j < aluno.numMat; j++)
+    {
+        __fpurge(stdin);
+        printf("Digite o nome da materia %d:", j + 1);
+        gets(strAux);
+        __fpurge(stdin);
+        aluno.grade[j].nome_mat = (char *)malloc((strlen(strAux) + 1) * sizeof(char));
+        strcpy(aluno.grade[j].nome_mat, strAux);
+        for(k = 0; k < NT; k++)
         {
-            __fpurge(stdin);
-            printf("Digite o nome do aluno %d\n", i + 1);
-            gets(strAux);
-            __fpurge(stdin);
-            info24[i].nome = (char *)malloc((strlen(strAux) + 1) * sizeof(char));
-            strcpy(info24[i].nome, strAux);
-            printf("Digite o numero de disciplinas: ");
-            scanf("%d", &info24[i].numMat);
-            info24[i].grade = (TMateria *)malloc(info24[i].numMat * sizeof(TMateria));
-            for(j = 0; j < info24[i].numMat; j++)
+            printf("Digite a nota %d ", k + 1);
+            scanf("%f", &aluno.grade[j].notas[k]);
+        }
+    }
+    return aluno;
+}
+
+void printaAluno()
+{
+    int i, j, k;
+    for (i = 0; i < numAluno; i++)
+    {
+        printf("NOME DO ALUNO: ");
+        puts(info24[i].nome);
+        for(j = 0; j < info24[i].numMat; j++)
+        {
+            printf("DESEMPENHO EM :");
+            puts(info24[i].grade[j].nome_mat);
+            for(k = 0; k < NT; k++)
             {
-                __fpurge(stdin);
-                printf("Digite o nome da materia %d:", j + 1);
-                gets(strAux);
-                __fpurge(stdin);
-                info24[i].grade[j].nome_mat = (char *)malloc((strlen(strAux) + 1) * sizeof(char));
-                strcpy(info24[i].grade[j].nome_mat, strAux);
-                for(k = 0; k < NT; k++)
-                {
-                    printf("Digite a nota %d ", k + 1);
-                    scanf("%f", &info24[i].grade[j].notas[k]);
-                }
+                printf("NOTAS: ");
+                printf("%.2f, ", info24[i].grade[j].notas[k]);
             }
         }
+        printf("\n");
     }
 }
